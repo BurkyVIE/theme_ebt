15ba09660718c13b4d5b1e0fbc4f4a8b56691c0a
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-04-19 08:07:32 +0200

    Create theme_ebt.r

---
+++ theme_ebt.r
@@ -0,0 +1,9 @@
+library(ggplot2)
+
+theme_ebt <- function()
+  theme_minimal() +
+  theme(legend.position = "top",
+        plot.title = element_text(size = 16, face = "bold", color = rgb(45, 56, 81, maxColorValue = 255)),
+        plot.background = element_rect(fill = rgb(221, 226, 233, maxColorValue = 255), color = NA),
+        strip.background = element_rect(fill = rgb(186, 194, 207, maxColorValue = 255), color = NA),
+        panel.grid = element_line(color = "white"))
