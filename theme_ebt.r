f13a122e78516693ef72d77e432127c2e339c594
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-03-03 19:16:48 +0100

    Update theme_ebt.r

--- theme_ebt.r
+++ theme_ebt.r
@@ -1,7 +1,7 @@
 library(ggplot2)
 
-theme_ebt <- function()
-  theme_minimal(base_size = 12) +
+theme_ebt <- function(base_size = 12, ...)
+  theme_minimal(...) +
   theme(legend.position = "top",
         plot.title = element_text(size = 16, face = "bold", color = rgb(45, 56, 81, maxColorValue = 255)),
         plot.background = element_rect(fill = rgb(221, 226, 233, maxColorValue = 255), color = NA),
